37f386982e6bb40ce88bfe6ac6b2e8df49bac0e4
Bianca Bensen <bianca06162007@gmail.com>
2026-07-02 18:08:24 -0700

    my text

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Hello, I'm Bianca Bensen
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm a second year student at the University of Washington in Seattle, majoring in Computer Science with a minor in Math.
+          I am currently working on building my portfolio and having fun with creating personal projects.
+          My goal is to gain experience working in the software development industry and learn about how AI is used on the job.
+          I have been interested in programming since I started high school, and with the rise of AI, it opens up a whole new frontier for me to explore.
+          In my free time, I enjoy swimming and playing piano, especially classical music. `}
       </p>
       <div className="my-8">
         <BlogPosts />
